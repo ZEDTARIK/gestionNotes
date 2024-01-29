0f917397c5637dc06f5cbe6272146aaff92247fe
zouhair.ettarak <ettarak.zouhair@gmail.com>
2024-01-29 16:09:18 +0100

    First Commit back end

---
+++ BackEnd/gestionNotes/src/main/java/org/ettarak/GestionNotesApplication.java
@@ -0,0 +1,13 @@
+package org.ettarak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionNotesApplication.class, args);
+	}
+
+}
