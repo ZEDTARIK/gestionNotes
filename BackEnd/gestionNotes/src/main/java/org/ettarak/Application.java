e0acc0b79d902503b89ce0528a86f2274f491690
zouhair.ettarak <ettarak.zouhair@gmail.com>
2024-01-29 16:17:00 +0100

    refactoring name class main

--- BackEnd/gestionNotes/src/main/java/org/ettarak/Application.java
+++ BackEnd/gestionNotes/src/main/java/org/ettarak/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GestionNotesApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GestionNotesApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
