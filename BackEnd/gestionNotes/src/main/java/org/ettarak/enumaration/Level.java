b511de2b84d04f1d67cc40bc9362838d9def78d9
zouhair.ettarak <ettarak.zouhair@gmail.com>
2024-01-29 16:32:54 +0100

    add Level Enum

--- BackEnd/gestionNotes/src/main/java/org/ettarak/enumaration/Level.java
+++ BackEnd/gestionNotes/src/main/java/org/ettarak/enumaration/Level.java
@@ -1,10 +1,17 @@
 package org.ettarak.enumaration;
 
 import lombok.Getter;
-
 @Getter
 public enum Level {
-    LOW,
-    MEDIUM,
-    HIGH;
+    LOW (1),
+    MEDIUM (2),
+    HIGH (3);
+
+    private  final  int level;
+    Level(int level) {
+        this.level = level;
+    }
+    public int getLevel() {
+        return level;
+    }
 }
