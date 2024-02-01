1871613a71a1150b7b08b227f7b5357e873e57aa
zouhair.ettarak <ettarak.zouhair@gmail.com>
2024-02-01 16:45:59 +0100

    HandleException custom

---
+++ BackEnd/gestionNotes/src/main/java/org/ettarak/exception/HandleException.java
@@ -0,0 +1,6 @@
+package org.ettarak.exception;
+
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+public class HandleException extends ResponseEntityExceptionHandler  {
+}
