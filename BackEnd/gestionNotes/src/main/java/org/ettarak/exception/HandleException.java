cab8083441de2a3b29c5a14e95fe83ddf93bf6ab
zouhair.ettarak <ettarak.zouhair@gmail.com>
2024-02-02 16:24:33 +0100

    Exception HAndle Error customer,  Filed cannot be Null or Empty

--- BackEnd/gestionNotes/src/main/java/org/ettarak/exception/HandleException.java
+++ BackEnd/gestionNotes/src/main/java/org/ettarak/exception/HandleException.java
@@ -1,6 +1,43 @@
 package org.ettarak.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.ettarak.models.HttpResponse;
+import org.ettarak.utils.DateUtil;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+@Slf4j
 public class HandleException extends ResponseEntityExceptionHandler  {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        log.error(exception.getMessage());
+
+        List<FieldError> fieldErrors= exception.getBindingResult().getFieldErrors();
+        String fieldMessage= fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(", "));
+
+        return new ResponseEntity<>(
+                HttpResponse.builder()
+                        .reason("Invalid fields: " + fieldMessage)
+                        .developerMessage(exception.getMessage())
+                        .statusCode(status.value())
+                        .status((HttpStatus) status)
+                        .timeStamp(LocalDateTime.now().format(DateUtil.dateTimeFormatter()))
+                        .build(),
+                status);
+
+    }
+
 }
