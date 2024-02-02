b67a454658f602d3ce80979590bd25fc50b0deaa
zouhair.ettarak <ettarak.zouhair@gmail.com>
2024-02-02 17:05:07 +0100

    Handle Exception Internal Error custom

--- BackEnd/gestionNotes/src/main/java/org/ettarak/exception/HandleException.java
+++ BackEnd/gestionNotes/src/main/java/org/ettarak/exception/HandleException.java
@@ -1,5 +1,6 @@
 package org.ettarak.exception;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.ettarak.models.HttpResponse;
 import org.ettarak.utils.DateUtil;
@@ -7,9 +8,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -37,7 +40,19 @@ public class HandleException extends ResponseEntityExceptionHandler  {
                         .timeStamp(LocalDateTime.now().format(DateUtil.dateTimeFormatter()))
                         .build(),
                 status);
-
     }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception exception, @Nullable Object body, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        return new ResponseEntity<>(
+                HttpResponse.builder()
+                        .reason("Internal Error " + exception.getMessage())
+                        .developerMessage(exception.getMessage())
+                        .statusCode(status.value())
+                        .status((HttpStatus) status)
+                        .timeStamp(LocalDateTime.now().format(DateUtil.dateTimeFormatter()))
+                        .build(),
+                status);
+        }
 
 }
