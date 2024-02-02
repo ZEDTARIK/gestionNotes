1f0256152f2c534df73b2a13002600252f437760
zouhair.ettarak <ettarak.zouhair@gmail.com>
2024-02-02 23:15:47 +0100

    Handle Exception Internal Error custom

--- BackEnd/gestionNotes/src/main/java/org/ettarak/exception/HandleException.java
+++ BackEnd/gestionNotes/src/main/java/org/ettarak/exception/HandleException.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import org.springframework.web.servlet.resource.NoResourceFoundException;
+
 
 import java.time.LocalDateTime;
 import java.util.List;
