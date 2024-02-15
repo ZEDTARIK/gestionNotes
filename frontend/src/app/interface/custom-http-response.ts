5ebb8ef6c8f841744967f259a9545deb10255843
zouhair.ettarak <ettarak.zouhair@gmail.com>
2024-02-15 17:20:09 +0100

    Add interface CustomHttpResponse

--- frontend/src/app/interface/custom-http-response.ts
+++ frontend/src/app/interface/custom-http-response.ts
@@ -1,2 +1,11 @@
+import { Note } from "./note";
+
 export interface CustomHttpResponse {
+  timeStamp: Date;
+  statucCode: number;
+  status: string;
+  message?: string;
+  reason?: string;
+  developerMessage?: string;
+  notes?: Note[];
 }
