28d92ed0f9b5b612334701b55278b33b55eb838a
zouhair.ettarak <ettarak.zouhair@gmail.com>
2024-02-20 16:04:14 +0100

    add CORS to backend

--- BackEnd/gestionNotes/src/main/java/org/ettarak/Application.java
+++ BackEnd/gestionNotes/src/main/java/org/ettarak/Application.java
@@ -1,13 +1,35 @@
 package org.ettarak;
 
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Collections.singletonList("http://localhost:4200/")); // Adjust as needed
+		config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "JWT-Token", "Authorization", "X-Requested-With", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		config.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "JWT-Token", "Authorization", "Access-Control-Allow-Credentials", "FileName"));
+		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source); // Passing the source to the CorsFilter constructor
 	}
 
 }
