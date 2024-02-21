3fef362778217a695fd143937334fba755d44b35
zouhair.ettarak <ettarak.zouhair@gmail.com>
2024-02-21 11:27:45 +0100

    add logique to SwitchCase DataState

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -14,6 +14,7 @@ import { catchError, map, startWith } from 'rxjs/operators';
 export class AppComponent implements OnInit {
 
   appState$: Observable<AppState<CustomHttpResponse>> | undefined;
+  readonly DataState = DataState;
 
   constructor(private noteService: NoteService) {}
 
